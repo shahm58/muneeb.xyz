89f525c6fe29924b59b330cb08969102f16408a8
Muneeb Shah <90660565+shahm58@users.noreply.github.com>
2022-02-04 15:52:37 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+
+  })
+
+})
+
+document.querySelectorAll(".collapse__button").forEach(button => {
+  button.addEventListener("click", () => {
+    const collapseContent = button.nextElementSibling;
+    button.classList.toggle("collapse__button--active");
+    if (button.classList.contains("collapse__button--active")) {
+      collapseContent.style.maxHeight = collapseContent.scrollHeight + "px";
+
+
+    } else {
+      collapseContent.style.maxHeight = 0;
+    }
+
+  });
+
+});
+
+var mybutton = document.getElementById("to__top");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function scrollBar(){
+  var scroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight-document.documentElement.clientHeight; //return viewable height
+  var scrolled = (scroll/height)*100
+  document.getElementById("indicator").style.width = scrolled + "%";
+}
+window.onscroll = function(){
+  scrollBar();
+}
